01f0eb684ffdcc17b1faa218b77c41fa38b9a2c7
yushiang <tsengyushiang@gmail.com>
2023-06-03 15:22:20 +0800

    fix: upper-case lower-case import issue.

--- three/index.js
+++ three/index.js
@@ -1,4 +1,4 @@
-import Hooks from "./Hooks";
+import hooks from "./hooks";
 import THREEPanoramaOutline from "./components/PanoramaOutline";
 import THREEPanoramaRoom from "./components/PanoramaRoom";
 import Canvas from "./components/ThreeCanvas";
@@ -11,7 +11,7 @@ export const PanoramaOutline = THREEPanoramaOutline;
 export const PanoramaRoom = THREEPanoramaRoom;
 
 // React Hooks
-export const Loaders = Hooks;
+export const Loaders = hooks;
 
 // THREE Algorithm
 export const Core = { Math, findIntersectionOfXZPlane };
