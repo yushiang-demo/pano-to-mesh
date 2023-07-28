7910da065a51a69d3fb7212aacbac63ba63921cc
yushiang <tsengyushiang@gmail.com>
2023-07-28 23:08:45 +0800

    doc: remove images from project use url.

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import Editor from "../apps";
 
 const Apps = () => {
-  return <Editor src={"/images/panoramas/1.jpeg"} />;
+  return <Editor src={""} />;
 };
 
 export default Apps;
