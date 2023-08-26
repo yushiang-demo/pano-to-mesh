56081cc3d46294b4917cd541f60a153abc16e949
yushiang <tsengyushiang@gmail.com>
2023-08-26 21:40:39 +0800

    enhnace: floating toolbar style.

--- components/Toolbar/index.js
+++ components/Toolbar/index.js
@@ -6,7 +6,6 @@ const Wrapper = styled.div`
   padding: 5px;
   align-items: center;
   position: relative;
-  box-shadow: 2px 2px 4px rgba(0, 0, 0, 0.2);
 `;
 const Toolbar = ({ children }) => {
   return <Wrapper>{children}</Wrapper>;
--- components/ToolbarRnd/index.js
+++ components/ToolbarRnd/index.js
@@ -1,8 +1,21 @@
 import { Rnd } from "react-rnd";
+import { styled } from "styled-components";
+
+const Wrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  background: #e2e2e2;
+  border-radius: 5px;
+  box-shadow: 2px 2px 4px rgba(0, 0, 0, 0.2);
+`;
 
 const ToolbarRnd = ({ children }) => {
   return (
     <Rnd
+      default={{
+        x: 10,
+        y: 10,
+      }}
       enableResizing={{
         top: false,
         right: false,
@@ -14,7 +27,7 @@ const ToolbarRnd = ({ children }) => {
         topLeft: false,
       }}
     >
-      {children}
+      <Wrapper>{children}</Wrapper>
     </Rnd>
   );
 };
