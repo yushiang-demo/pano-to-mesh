120613f1038b3f75a3ecb37bb84381381fb11538
yushiang <tsengyushiang@gmail.com>
2023-10-07 16:02:06 +0800

    enhnace: enable iframe pointer-events when focused.

--- components/MediaManager/Iframe/styled.js
+++ components/MediaManager/Iframe/styled.js
@@ -4,4 +4,15 @@ export const Iframe = styled.iframe`
   width: 100%;
   height: 100%;
   background: white;
+  pointer-events: none;
+`;
+
+export const PointerEvents = styled.div`
+  width: 100%;
+  height: 100%;
+  &:focus-within {
+    ${Iframe} {
+      pointer-events: auto;
+    }
+  }
 `;
