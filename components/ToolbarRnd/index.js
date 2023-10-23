32364374d245e628cde095788ad40afba269ee1c
yushiang <tsengyushiang@gmail.com>
2023-10-24 00:31:34 +0800

    fix: support mobile touch events

--- components/ToolbarRnd/index.js
+++ components/ToolbarRnd/index.js
@@ -1,35 +1,16 @@
-import { Rnd } from "react-rnd";
 import { styled } from "styled-components";
 
 const Wrapper = styled.div`
-  width: 100%;
-  height: 100%;
+  position: fixed;
+  z-index: 999;
+  top: 0;
   background: #e2e2e2;
   border-radius: 5px;
   box-shadow: 2px 2px 4px rgba(0, 0, 0, 0.2);
 `;
 
 const ToolbarRnd = ({ children }) => {
-  return (
-    <Rnd
-      default={{
-        x: 10,
-        y: 10,
-      }}
-      enableResizing={{
-        top: false,
-        right: false,
-        bottom: false,
-        left: false,
-        topRight: false,
-        bottomRight: false,
-        bottomLeft: false,
-        topLeft: false,
-      }}
-    >
-      <Wrapper>{children}</Wrapper>
-    </Rnd>
-  );
+  return <Wrapper>{children}</Wrapper>;
 };
 
 export default ToolbarRnd;
