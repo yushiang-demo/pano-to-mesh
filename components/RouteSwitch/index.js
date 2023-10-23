43b1f8869af7bc83cce20a099ca7011d70d2e2c0
yushiang <tsengyushiang@gmail.com>
2023-10-23 18:11:29 +0800

    feature: add float menu to redirect between editor and viewer.

---
+++ components/RouteSwitch/index.js
@@ -0,0 +1,63 @@
+import { useRouter } from "next/router";
+import Icons from "../Icon";
+import { FloatDiv, Item } from "./styled";
+import { useEffect, useState } from "react";
+
+const RouterSwitch = ({ data }) => {
+  const router = useRouter();
+
+  const extendsLink = (link) => {
+    const path = new URL(link, window.location.href);
+    path.hash = window.location.hash;
+    return path;
+  };
+
+  const redirectTo = (link) => {
+    return () => {
+      router.push(extendsLink(link));
+    };
+  };
+
+  const needsHighlight = (link) => {
+    return router.pathname === link;
+  };
+
+  return (
+    <FloatDiv>
+      {data.map(({ link, Icon }, index) => (
+        <Item key={index} onClick={redirectTo(link)}>
+          <Icon $highlight={needsHighlight(link)} />
+        </Item>
+      ))}
+    </FloatDiv>
+  );
+};
+
+const DataWrapper = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    setData([
+      {
+        link: "/editors/layout2d",
+        Icon: Icons.panorama,
+      },
+      {
+        link: "/editors/layout3d",
+        Icon: Icons.cube,
+      },
+      {
+        link: "/editors/decoration",
+        Icon: Icons.arrange,
+      },
+      {
+        link: "/",
+        Icon: Icons.preview,
+      },
+    ]);
+  }, []);
+
+  return <RouterSwitch data={data} />;
+};
+
+export default DataWrapper;
